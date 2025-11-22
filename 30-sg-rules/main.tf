3b7628117f0f2bcefcfb5dc5e698bbe588fa10e0
pennabadiumamaheswarreddy <pennabadiumamaheswarreddy@gmail.com>
2025-11-22 09:01:34 +0530

    terraform

--- 30-sg-rules/main.tf
+++ 30-sg-rules/main.tf
@@ -52,11 +52,20 @@ resource "aws_security_group_rule" "mysql_bastion" {
   to_port           = 22
 }
 
-resource "aws_security_group_rule" "catalogue" {
+resource "aws_security_group_rule" "catalogue_bastion" {
   type              = "ingress"
   security_group_id = local.catalogue_sg_id
   source_security_group_id = local.bastion_sg_id
   from_port         = 22
+  protocol          = "tcp"
+  to_port           = 22
+}
+
+resource "aws_security_group_rule" "mongodb_catalogue" {
+  type              = "ingress"
+  security_group_id = local.mongodb_sg_id
+  source_security_group_id = local.catalogue_sg_id
+  from_port         = 27027
   protocol          = "tcp"
   to_port           = 22
 }
