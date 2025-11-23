a41b9e123113400e9a4ece53c0ed53c443115252
pennabadiumamaheswarreddy <pennabadiumamaheswarreddy@gmail.com>
2025-11-23 16:42:43 +0530

    terraform

--- 60-catalogue/main.tf
+++ 60-catalogue/main.tf
@@ -1,16 +1,16 @@
-# Create ec2 instance 
+# Create EC2 instance
 resource "aws_instance" "catalogue" {
-  ami = local.ami_id
-  instance_type = "t3.micro"
-  vpc_security_group_ids = [local.catalogue_sg_id]
-  subnet_id = local.private_subnet_ids
-  
-  tags = merge (
-    local.common_tags,
-    {
-        Name = "${local.common_name_suffix}-catalogue" #roboshop-dev-catalogue
-    }
-  )
+    ami = local.ami_id
+    instance_type = "t3.micro"
+    vpc_security_group_ids = [local.catalogue_sg_id]
+    subnet_id = local.private_subnet_id
+    
+    tags = merge (
+        local.common_tags,
+        {
+            Name = "${local.common_name_suffix}-catalogue" # roboshop-dev-mongodb
+        }
+    )
 }
 
 # Connect to instance using remote-exec provisioner through terraform_data
