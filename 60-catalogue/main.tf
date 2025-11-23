ea265ac28a2da609b465280c653e6cf36c17fa9a
pennabadiumamaheswarreddy <pennabadiumamaheswarreddy@gmail.com>
2025-11-23 16:40:21 +0530

    terraform

--- 60-catalogue/main.tf
+++ 60-catalogue/main.tf
@@ -1,10 +1,9 @@
 # Create ec2 instance 
-
 resource "aws_instance" "catalogue" {
   ami = local.ami_id
   instance_type = "t3.micro"
   vpc_security_group_ids = [local.catalogue_sg_id]
-  subnet_id = local.private_subnet_id
+  subnet_id = local.private_subnet_ids
   
   tags = merge (
     local.common_tags,
