aff53a684f519355353a0da8ac906737b80994fb
pennabadiumamaheswarreddy <pennabadiumamaheswarreddy@gmail.com>
2025-11-30 00:32:58 +0530

    terraform

--- 50-backend-alb/main.tf
+++ 50-backend-alb/main.tf
@@ -31,3 +31,17 @@ resource "aws_lb_listener" "front_end" {
     }
   }
 }
+
+resource "aws_route53_record" "backend_alb" {
+  zone_id = var.zone_id
+  name    = "*.backend-alb-${var.environment}.${var.domain_name}"
+  type    = "A"
+
+  alias {
+    #These are alb details, not our details
+    name                   = aws_lb.backend_alb.dns_name
+    zone_id                = aws_lb.backend_alb.zone_id
+    evaluate_target_health = true
+  }
+}
+
--- 50-backend-alb/variables.tf
+++ 50-backend-alb/variables.tf
@@ -5,3 +5,11 @@ variable "project_name"{
 variable "environment"{
     default = "dev"
 }
+
+variable "zone_id"{
+    default = "Z01824481BRLAZZOTKAWJ"
+}
+
+variable "domain_name"{
+    default = "umamaheswarreddy.fun"
+}
