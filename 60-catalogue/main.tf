35dc7aa99874663a82bc09070c31b7830d50d4fe
pennabadiumamaheswarreddy <pennabadiumamaheswarreddy@gmail.com>
2025-11-30 00:43:34 +0530

    terraform

--- 60-catalogue/main.tf
+++ 60-catalogue/main.tf
@@ -88,6 +88,9 @@ resource "aws_launch_template" "catalogue" {
 
   vpc_security_group_ids = [local.catalogue_sg_id]
 
+  # when we run terraform apply again, a new version will be created with new AMI ID
+  update_default_version = true
+
   #tags attached to the instance
   tag_specifications {
     resource_type = "instance"
