1a4690e0b499cceab9742f53ac6119a0323ae4a4
pennabadiumamaheswarreddy <pennabadiumamaheswarreddy@gmail.com>
2025-12-01 17:48:59 +0530

    terraform

--- 40-databases/main.tf
+++ 40-databases/main.tf
@@ -135,7 +135,7 @@ resource "aws_instance" "mysql" {
   )
 }
 
-resource "aws_iam_instance_profile" "mysql1"{
+resource "aws_iam_instance_profile" "mysql"{
   name = "mysql"
   role = "EC2SSMParameterRead"
 }
