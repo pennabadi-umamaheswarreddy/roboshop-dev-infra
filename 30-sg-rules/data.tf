8af09cb7c8998c865bc7531cd3b387c20287e8db
pennabadiumamaheswarreddy <pennabadiumamaheswarreddy@gmail.com>
2025-12-05 22:04:01 +0530

    terraform

--- 30-sg-rules/data.tf
+++ 30-sg-rules/data.tf
@@ -2,8 +2,8 @@ data "aws_ssm_parameter" "backend_alb_sg_id" {
   name = "/${var.project_name}/${var.environment}/backend_alb_sg_id"
 }
 
-data "aws_ssm_parameter" "frontend_lb_sg_id" {
-  name = "/${var.project_name}/${var.environment}/frontend_lb_sg_id"
+data "aws_ssm_parameter" "frontend_alb_sg_id" {
+  name = "/${var.project_name}/${var.environment}/frontend_alb_sg_id"
 }
 
 data "aws_ssm_parameter" "bastion_sg_id" {
@@ -28,4 +28,24 @@ data "aws_ssm_parameter" "mysql_sg_id" {
 
 data "aws_ssm_parameter" "catalogue_sg_id" {
   name = "/${var.project_name}/${var.environment}/catalogue_sg_id"
+}
+
+data "aws_ssm_parameter" "user_sg_id" {
+  name = "/${var.project_name}/${var.environment}/user_sg_id"
+}
+
+data "aws_ssm_parameter" "cart_sg_id" {
+  name = "/${var.project_name}/${var.environment}/cart_sg_id"
+}
+
+data "aws_ssm_parameter" "shipping_sg_id" {
+  name = "/${var.project_name}/${var.environment}/shipping_sg_id"
+}
+
+data "aws_ssm_parameter" "payment_sg_id" {
+  name = "/${var.project_name}/${var.environment}/payment_sg_id"
+}
+
+data "aws_ssm_parameter" "frontend_sg_id" {
+  name = "/${var.project_name}/${var.environment}/frontend_sg_id"
 }
